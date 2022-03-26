cbb5136f0a02c321b595acc3a9dfe52e2287d5cb
Đức <64053044+steveho29@users.noreply.github.com>
2022-03-26 23:51:38 +0700

    Create CalculateAge.kt

---
+++ app/src/main/java/com/ZellyCookies/PineApple/Utils/CalculateAge.kt
@@ -0,0 +1,25 @@
+package com.ZellyCookies.PineApple.Utils
+
+import java.util.*
+
+//Aging methods
+class CalculateAge(dob: String) {
+    var age = 0
+
+
+    fun setAge(year: Int, month: Int, day: Int) {
+        val dateOfBirth = Calendar.getInstance()
+        val today = Calendar.getInstance()
+        dateOfBirth[year, month] = day
+        var age = today[Calendar.YEAR] - dateOfBirth[Calendar.YEAR]
+        if (today[Calendar.DAY_OF_YEAR] < dateOfBirth[Calendar.DAY_OF_YEAR]) {
+            age--
+        }
+        this.age = age
+    }
+
+    init {
+        val splitDOB = dob.split("-").toTypedArray()
+        setAge(splitDOB[2].toInt(), splitDOB[0].toInt(), splitDOB[1].toInt())
+    }
+}
