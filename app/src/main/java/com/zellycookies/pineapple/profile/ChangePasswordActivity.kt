d29e72821d77e13349ed8cbb4811f154561e0f11
Tung Tran <56991132+C2H4Br2@users.noreply.github.com>
2022-03-30 19:30:28 +0700

    Added change password activity

--- app/src/main/java/com/zellycookies/pineapple/profile/ChangePasswordActivity.kt
+++ app/src/main/java/com/zellycookies/pineapple/profile/ChangePasswordActivity.kt
@@ -1,59 +1,25 @@
 package com.zellycookies.pineapple.profile
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageButton
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-import com.zellycookies.pineapple.login.Login
-import com.zellycookies.pineapple.R
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseAuth.AuthStateListener
+import com.zellycookies.pineapple.R
+import com.zellycookies.pineapple.login.Login
 
-class SettingsActivity : AppCompatActivity() {
+class ChangePasswordActivity : AppCompatActivity() {
     //firebase
     private var mAuth: FirebaseAuth? = null
-    private var mAuthListener: AuthStateListener? = null
+    private var mAuthListener: FirebaseAuth.AuthStateListener? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_settings)
+        setContentView(R.layout.activity_change_password)
 
         //setup firebase
         setupFirebaseAuth()
-
-        //setup display content
-        buttonSetup()
-        val toolbar = findViewById<View>(R.id.toolbartag) as TextView
-        toolbar.text = "Profile"
-
-        //setup buttons
-        val back = findViewById<View>(R.id.back) as ImageButton
-        back.setOnClickListener { onBackPressed() }
-    }
-
-    private fun buttonSetup() {
-        findViewById<Button>(R.id.btn_logout).setOnClickListener {
-            funLogout()
-        }
-        findViewById<Button>(R.id.btn_changePassword).setOnClickListener {
-            funChangePassword()
-        }
-    }
-
-    //Logout/Signout
-    private fun funLogout() {
-        mAuth!!.signOut()
-        finish()
-    }
-
-    // Change password
-    private fun funChangePassword() {
-        val intent = Intent(this@SettingsActivity, ChangePasswordActivity::class.java)
-        startActivity(intent)
-        finish()
     }
 
     //----------------------------------------Firebase----------------------------------------
@@ -62,7 +28,7 @@ class SettingsActivity : AppCompatActivity() {
      */
     private fun setupFirebaseAuth() {
         mAuth = FirebaseAuth.getInstance()
-        mAuthListener = AuthStateListener { firebaseAuth ->
+        mAuthListener = FirebaseAuth.AuthStateListener { firebaseAuth ->
             val user = firebaseAuth.currentUser
             if (user != null) {
                 // user is signed in
@@ -71,7 +37,7 @@ class SettingsActivity : AppCompatActivity() {
                 //user is signed out
                 Log.d(TAG, "onAuthStateChanged: signed_out")
                 Log.d(TAG, "onAuthStateChanged: navigating back to login screen.")
-                val intent = Intent(this@SettingsActivity, Login::class.java)
+                val intent = Intent(this@ChangePasswordActivity, Login::class.java)
 
                 //clear the activity stack， in case when sign out, the back button will bring the user back to the previous activity
                 startActivity(intent)
@@ -93,6 +59,6 @@ class SettingsActivity : AppCompatActivity() {
     }
 
     companion object {
-        private const val TAG = "SettingsActivity"
+        private const val TAG = "ChangePasswordActivity"
     }
 }
