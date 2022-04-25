736e4ef5ea70bbdab1391f70d17434810c509edf
Đức <64053044+steveho29@users.noreply.github.com>
2022-04-25 16:52:54 +0700

    allows ACCESS_NETWORK_STATE

---
+++ app/src/main/java/com/zellycookies/pineapple/news/News_Activity.kt
@@ -0,0 +1,13 @@
+package com.zellycookies.pineapple.news
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class News_Activity: AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
