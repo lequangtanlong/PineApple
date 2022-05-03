60881f698b271cad009f99f266458b9c1096387b
Đức <64053044+steveho29@users.noreply.github.com>
2022-05-03 21:39:22 +0700

    Init NewsActivity app compact

--- app/src/main/java/com/zellycookies/pineapple/news/News_Activity.kt
+++ app/src/main/java/com/zellycookies/pineapple/news/News_Activity.kt
@@ -1,13 +1,30 @@
 package com.zellycookies.pineapple.news
 
+import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import com.zellycookies.pineapple.R
 
-class News_Activity: AppCompatActivity() {
 
 
+
+class NewsActivity: AppCompatActivity() {
+    private var articles: ArrayList<News>? = fetchNews()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_news)
+
+        var newsRecyclerView = findViewById<RecyclerView>(R.id.newsRecyclerView)
+//        var adapter = NewsListAdapter(this, articles)
     }
+
 }
+
+
