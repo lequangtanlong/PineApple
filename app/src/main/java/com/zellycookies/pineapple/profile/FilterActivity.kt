baea0e58627fcf7a8010a510462fdeae734bc6da
Lê Quang Tấn Long <63658369+lequangtanlong@users.noreply.github.com>
2022-05-03 10:59:22 +0700

    Update Filter activity

--- app/src/main/java/com/zellycookies/pineapple/profile/FilterActivity.kt
+++ app/src/main/java/com/zellycookies/pineapple/profile/FilterActivity.kt
@@ -1,22 +1,28 @@
 package com.zellycookies.pineapple.profile
 
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.SeekBar
+import android.view.View
+import android.widget.*
 import android.widget.SeekBar.OnSeekBarChangeListener
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar
-import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar.OnRangeSeekBarChangeListener
 import com.zellycookies.pineapple.R
+import com.zellycookies.pineapple.home.HomeSwipeActivity
+import com.zellycookies.pineapple.main.ProfileCheckinMain
+
 
 class FilterActivity : AppCompatActivity() {
     private val TAG = "SettingsActivity"
     private lateinit var distance: SeekBar
     private lateinit var rangeSeekBar: RangeSeekBar<*>
-    private lateinit var gender: TextView
-    private lateinit var distanceText:TextView
-    private lateinit var ageRange:TextView
+    private lateinit var gender : TextView
+    private lateinit var distanceText :TextView
+    private lateinit var ageRange :TextView
+    private lateinit var genderSpinner : Spinner
+    private lateinit var submitButton : Button
+    private var maxDistance : Int = 100
+    private var minDistance : Int = 16
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,11 +30,31 @@ class FilterActivity : AppCompatActivity() {
         val toolbar = findViewById<TextView>(R.id.toolbartag)
         toolbar.text = "Filter"
         val back = findViewById<ImageButton>(R.id.back)
+        gender = findViewById(R.id.gender_text)
         distance = findViewById(R.id.distance)
-
         distanceText = findViewById<TextView>(R.id.distance_text)
         ageRange = findViewById<TextView>(R.id.age_range)
         rangeSeekBar = findViewById(R.id.rangeSeekbar)
+        genderSpinner = findViewById(R.id.gender_spinner)
+        submitButton = findViewById(R.id.submitButton)
+
+        val genderPreferences = resources.getStringArray(R.array.gender_preferences)
+        if (genderSpinner != null) {
+            val adapter = ArrayAdapter(this,
+                android.R.layout.simple_spinner_item, genderPreferences)
+            genderSpinner.adapter = adapter
+
+            genderSpinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>, view: View, position: Int, id: Long) {
+                    gender.text = genderPreferences[position].toString()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                }
+            }
+        }
+
         distance.setOnSeekBarChangeListener(object : OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar, progress: Int, fromUser: Boolean) {
                 distanceText.text = "$progress Km"
@@ -39,7 +65,19 @@ class FilterActivity : AppCompatActivity() {
         })
 
         rangeSeekBar.setOnRangeSeekBarChangeListener { bar, number, number2 ->
-            ageRange.setText("$number - $number2")
+            ageRange.text = "$number - $number2"
+            maxDistance = number2.toInt()
+            minDistance = number.toInt()
+        }
+
+        submitButton.setOnClickListener {
+            val intent = Intent(this@FilterActivity, HomeSwipeActivity::class.java)
+            intent.putExtra("genderPreference", gender.text)
+            intent.putExtra("distance", distance.progress)
+            intent.putExtra("minAge", minDistance)
+            intent.putExtra("maxAge", maxDistance)
+
+            onBackPressed()
         }
 
         back.setOnClickListener { onBackPressed() }
